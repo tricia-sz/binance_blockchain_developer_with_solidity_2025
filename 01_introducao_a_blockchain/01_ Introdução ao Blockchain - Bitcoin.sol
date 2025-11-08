3e88a38798031ee5cdf8ef90deba84d26e4b73f8
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-08 01:29:18 -0300

    intro a blockchain - bitcoin

---
+++ 01_introducao_a_blockchain/01_ Introdução ao Blockchain - Bitcoin.sol
@@ -0,0 +1,72 @@
+BLOCK CHAIN:
+
+Banco de dados distriuido com registros de todas as trasansações que foram executados e compartilhas entre participantes.
+Todo os computadores da rede possuem uma cópia idêntica do livro de trasansações.
+
+DADOS DISTRÍBUIDOS:
+Não existe um servidor ou um sistema central que mantenha os dados da Blcokchain.
+Os dados são distribídos por  milhões de computadores em todo mundo que estão conectados a Blcokchain.
+
+GASTO DUPLO:
+Gastar duas vezes é possível com dinheiro físico, como modedas e notas. Na Blcokchain isso simplesmente não é possivel, por tanto, não é um problema.
+
+COMO FUNCIONA A TECNOLOGIA BLOCKCHAIN?
+Um dos usos famosos da Blcokchain é o Bitcoin.
+O Bitcoin usa prova criptografica ao invez terceiros para que duas partes executem trasansações pela internet.
+
+
+NODES:
+
+Um computador conectado a rede. Quanto mais NODES existem, mais a rede fica DESCENTRALIZADA. 
+Em reusmo: Validador de transações.
+
+FULL NODES: 
+
+Responsável por retransmitir as novas transações e blocos.
+
+Um full Node faz donwload de uma cópia do blockchain do Bitcoin contendo todo o histórico da transação.
+
+Todas as trasansações estão nos FULL-NODES desde a criação do bitcoin.
+
+MINING-NODE:
+
+Responsável por agrupar as transações para ctiar um bloco candidato à mineração.
+
+Isso ocorre após a aprovação do FULL-NODE.
+
+Se conseguir resolver o problema matemático, ele consegue uma recompensa em bitcoin.
+
+Volta para o FULL-NODE e adciona o bloco na blockchain.
+
+
+LIGHT-NODE
+Responsável por armazenar o histórico do que já foi aprovado pelo Full-Node, é um auxiliar.
+
+SUPER-NODE:
+
+Tem a mesma função do FULL NODE, no entanto, com maior capacidade de verificar e aprovar as transações.
+
+Hoje temos 14416 NODES, segundo ao Bitnodes.
+
+BLOCKCHAIN NÃO É SÓ BITCOIN!
+
+Bitcoin é uma aplicação da tecnologia blockchain.
+
+Existem muitos outros apps e casos de uso que podem ser resolvidos usando blockchain além de eco sustemas de pagamento.
+
+Ex: Rastreabilidade - Certificação - Seguros -Complaince - Identidade - ETC, ETC...
+
+
+BENEFÍCIOS DA TECNOLOGIA BLOCKCHAIN:
+
+Economia de tempo: Nenhuma verificação de outra autoridade central necessária para liquidações, tornando o processo mais rápido e barato.
+
+Economia de custos: uma rede Blcokchain reduz a despesas de várias maneiras. Não há necessidade de verificações de terceitos. Os participantes podem
+compariticar ativos diretamente. Os intermediarios são reduzidos.
+
+Segurança nas rígida: Ninguém pode alterar dados da blockchain, pois é compartilhado entre milhões de participantes. O ssistema é seguro contra crimes cibernéticos e fraudes.
+
+
+
+
+
